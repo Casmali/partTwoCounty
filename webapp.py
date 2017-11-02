5536dc729daea366c52ad11e36d013a10e72027f
f.c.lopez <casmali.f.lopez@gmail.com>
2017-11-02 08:59:54 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,14 +1,25 @@
-from flask import Flask, url_for, render_template, request
+from flask import Flask, request Markup, render_template, flash, Markup
+import os
+import json
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
 def main():
-    with open('county_demographics.json') as demographics_data:
+    with open('static/county_demographics.json') as demographics_data:
         gloal counties = json.load(demographics_data)
 
 @app.route("/")
 def render_main():
-    return render_template('index.html', )
+    ret = ""
+    for i in counties:
+        ret += Markup("<option value=\"" + i[State] + "\">" + i[State] + "</option>")
+    return render_template('index.html',getStateOptions())
+
+def getStateOptions():
+    ret = ""
+    for i in counties:
+        ret += Markup("<option value=\"" + i[State] + "\">" + i[State] + "</option>")
+    return ret
 
 @app.route("/response")
 def render_response():
