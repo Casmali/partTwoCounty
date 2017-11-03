bd6f5c78a90679912f6547436acd9eb08926ef45
f.c.lopez <casmali.f.lopez@gmail.com>
2017-11-03 09:13:42 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -18,12 +18,11 @@ def getStateOptions(counties):
             ret += Markup("<option value=\"" + i[State] + "\">" + i[State] + "</option>")
     return ret
 
-@app.route("/response")
-def render_response():
-        ins = float(request.args['submit'])
-        #The request object stores information that was sent by the client to the server.
-        #the args is a multidict
-        #the way we get info from args is that it is visible in a url. - the information in args is visible in the url for hte page being requested(ex. .../response?color=blue)
-        
-        return render_template('response.html', their = request.args['inches'], response = res)
+#@app.route("/response")
+#def render_response():
+#        ins = float(request.args['submit'])
+#        #The request object stores information that was sent by the client to the server.
+#        #the args is a multidict
+#        #the way we get info from args is that it is visible in a url. - the information in args is visible in the url for hte page being requested(ex. .../response?color=blue)
+#        return render_template('response.html', their = request.args['inches'], response = res)
 
