9b2a253eece4c5f9c4183db05f13481dd086b241
f.c.lopez <casmali.f.lopez@gmail.com>
2017-11-06 09:18:47 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -4,8 +4,8 @@ import json
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
-#with open(url_for("county_demographics.json") as demographics_data:
-#        counties = json.load(demographics_data)
+with open(url_for("county_demographics.json") as demographics_data:
+        counties = json.load(demographics_data)
 
 @app.route("/")
 def render_main():
