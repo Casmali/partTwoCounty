6c4d5b51ce6ba05f86e21895462a1573da44565a
f.c.lopez <casmali.f.lopez@gmail.com>
2017-11-06 09:58:53 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -20,16 +20,16 @@ def getStateOptions(counties):
             lis[i['State']] = True
     return ret
 
-#@app.route("/response")
-#def render_response():
-#        ins = float(request.args['state'])
-#        x = 0
-#        for i in counties:
-#                if i['state'] == ins:
-##                        x += i['Population']['2014 Population']
- #       y = "The populatin of all the counties in this state is: " + str(x)
- #       #The request object stores information that was sent by the client to the server.
- #       #the args is a multidict
- #       #the way we get info from args is that it is visible in a url. - the information in args is visible in the url for hte page being requested(ex. .../response?color=blue)
- #       return render_template('response.html', val = getStateOptions(counties), funfacts = y  )
+@app.route("/response")
+def render_response():
+        ins = float(request.args['state'])
+        x = 0
+        for i in counties:
+                if i['state'] == ins:
+                        x += i['Population']['2014 Population']
+        y = "The populatin of all the counties in this state is: " + str(x)
+        #The request object stores information that was sent by the client to the server.
+        #the args is a multidict
+        #the way we get info from args is that it is visible in a url. - the information in args is visible in the url for hte page being requested(ex. .../response?color=blue)
+        return render_template('response.html', val = getStateOptions(counties), funfacts = y  )
 
