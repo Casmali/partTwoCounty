2f8f26ac433fc6a0547bc6d1554c45d005b0cc13
f.c.lopez <casmali.f.lopez@gmail.com>
2017-11-07 09:19:17 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -31,5 +31,5 @@ def render_response():
         #The request object stores information that was sent by the client to the server.
         #the args is a multidict
         #the way we get info from args is that it is visible in a url. - the information in args is visible in the url for hte page being requested(ex. .../response?color=blue)
-        return render_template('response.html', val = getStateOptions(counties), funfacts = "isthiswork"  )
+        return render_template('response.html', val = getStateOptions(counties), funfact = y  )
 
